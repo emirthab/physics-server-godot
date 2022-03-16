aff8d1ff9eeb8cc37331635dda5b8c9eb08b0aa7
emirthab <gununoyuncusu@gmail.com>
2022-03-17 00:15:23 +0300

    manager

--- src/Player.cpp
+++ src/Player.cpp
@@ -45,9 +45,9 @@ void Player::setPing()
 	ServerManager Manager;
 	using namespace std::chrono;
 	uint64_t currentTime = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
-	Godot::print( "lastPingTime = " + godot::String(std::to_string((uint64_t)Manager.get("lastSendedPingTime")).c_str()));
+	Godot::print( "lastPingTime = " + godot::String(std::to_string(Manager.lastSendedPingTime).c_str()));
 	Godot::print("CurretnTime = " + godot::String(std::to_string(currentTime).c_str()));
-	PING = (uint64_t)Manager.get("lastSendedPingTime") - currentTime;
+	PING = Manager.lastSendedPingTime - currentTime;
 }
 
 void Player::locationDataRecognizer(float delta)
